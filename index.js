39b1f1f785897f66f6ab9f83cfd81b2a0c18e5da
Fishlennon <jpaviles5@gmail.com>
2024-01-07 17:21:47 -0300

    funcion index

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-buena
+function laCajaDePandora(numero) {
+    if (typeof numero !== 'number' || !Number.isInteger(numero)) {
+        return "Error";
+    }
+    if (numero % 2 === 0) {
+        return numero.toString(2);
+    } else {
+        return numero.toString(16);
+    }
+}
