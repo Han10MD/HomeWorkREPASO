d7a7ba41db3dd54ebbd757e0ccdd28d4bfa11599
Ezequiel Antoine <ezequielantoine@gmail.com>
2024-01-07 17:14:33 -0300

    commit ezequiel

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-buena
+function laCajaDePandora(numero){
+    if (numero % 2 === 0) {
+        // Convertimos el número a binario
+        return numero.toString(2);
+      } else {
+        // Convertimos el número a hexadecimal
+        return numero.toString(16);
+      }
+}
